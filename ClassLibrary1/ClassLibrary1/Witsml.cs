08447e176e97e496010d503ffd0c70b542fca349
arkady <asg19@tpu.ru>
2016-11-07 15:22:40 +0700

    first commit

---
+++ ClassLibrary1/ClassLibrary1/Witsml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    /// <summary>
+    /// Класс для описания сущности witsml-файла
+    /// </summary>
+    class Witsml
+    {
+
+    }
+}
